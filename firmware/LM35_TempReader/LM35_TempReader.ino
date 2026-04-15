5029bdb983e9498673803fe99a007f233ef885c5
Tuyet Minh <n23dcci045@student.otithcm.edu.vn>
2026-04-15 08:36:06 +0700

    feat: ho tro doc 2 kenh LM35 (A0, A1)

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,20 +1,23 @@
-
-/*
- * Du an: Doc nhiet do LM35 va gui qua Serial
- * Mon: Mang Cam Bien - Tuan 03
- */
-
-int adcValue;
-float nhietDo;
+int adcValues[2]; 
+float nhietDo[2]; 
+char chuoi[30];
 
 void setup() {
   Serial.begin(9600);
-  delay(100);
 }
 
 void loop() {
-  adcValue = analogRead(A0);
-  nhietDo = (adcValue * 500.0) / 1023.0;
-  Serial.println(nhietDo, 1);
-  delay(1000);
+  // Đọc kênh A0
+  adcValues[0] = analogRead(A0);
+  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+
+  // Đọc kênh A1
+  adcValues[1] = analogRead(A1);
+  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
+
+  // Gửi dữ liệu dạng CSV qua Serial
+  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
+  Serial.print(chuoi);
+
+  delay(100);
 }
