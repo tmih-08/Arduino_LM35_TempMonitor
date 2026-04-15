2e01a9b2b671f0d30ebee935bae23850e59f39d0
Tuyet Minh <n23dcci045@student.otithcm.edu.vn>
2026-04-15 15:13:58 +0700

    feat: doi dinh dang Serial sang JSON

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -15,8 +15,8 @@ void loop() {
   adcValues[1] = analogRead(A1);
   nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
 
-  // Gửi dữ liệu dạng CSV qua Serial
-  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
+  // SV B sửa: Gửi dữ liệu dạng JSON thay vì CSV
+  sprintf(chuoi, "{\"temp1\":%d, \"temp2\":%d}\n", (int)nhietDo[0], (int)nhietDo[1]);
   Serial.print(chuoi);
 
   delay(100);
