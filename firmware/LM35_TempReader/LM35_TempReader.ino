18eaba1ffe9e571e4185ef17220348d2e410c942
Tuyet Minh <n23dcci045@student.otithcm.edu.vn>
2026-04-15 21:02:40 +0700

    fix: update firmware to match teacher format (csv format)

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,25 +1,63 @@
-int adcValues[3];    
-float nhietDo[3];   
-char chuoi[100];    
 
-void setup() {
-  Serial.begin(9600);
+
+//khai bao bien toan cuc
+int adc[2];
+float Tam;
+byte NhietDo[2];
+char Chuoi[20];
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() 
+{
+  while (Serial.available()) 
+  {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+  
+    // do something about it:
+    if (inChar != '\n')
+    {
+      
+    }
+  }
 }
 
-void loop() {
-  // Đọc dữ liệu cả 3 kênh (Thừa hưởng từ SV A)
-  adcValues[0] = analogRead(A0);
-  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+void setup()
+{
+  // put your setup code here, to run once:
 
-  adcValues[1] = analogRead(A1);
-  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
+  Serial.begin(9600); delay(100);
+  
+}
 
-  adcValues[2] = analogRead(A2);
-  nhietDo[2] = (adcValues[2] * 500.0) / 1023.0;
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  /*
+   * ta co: cu 1023[adc] thi tuong ung 5000mv
+   * vay  voi adc thi tuong ung bao nhieu a mv?  
+   * => a = (adc * 5000)/1023
+   * theo datasheet ta co:
+   * cu  10mv thi tuong ung 1 do C
+   * vay (adc * 5000)/1023 thi tuong ung b do C
+   * => b = (adc * 5000)/1023/10 = (adc*500)/1023
+   */
 
-  // Gửi dữ liệu dạng JSON cho cả 3 kênh (Thừa hưởng ý tưởng SV B)
-  sprintf(chuoi, "{\"t1\":%d, \"t2\":%d, \"t3\":%d}\n", (int)nhietDo[0], (int)nhietDo[1], (int)nhietDo[2]);
-  Serial.print(chuoi);
-
+  adc[0] = analogRead(A0);
+  Tam =  (adc[0]*500.0)/1023.0; 
+  NhietDo[0] = Tam;
+  
+  adc[1] = analogRead(A1);
+  Tam =  (adc[1]*500.0)/1023.0; 
+  NhietDo[1] = Tam;  
+  
+  sprintf(Chuoi,"%d,%d\n",NhietDo[0],NhietDo[1]);
+  Serial.print(Chuoi);
   delay(100);
+   
+
 }
